3eaecb68531044d73805ed11717f3dbebfa7414f
coatlessali <coatlessali@protonmail.com>
2024-09-24 00:13:08 -0500

    Add ability to set directory Supposed to be set to the USRDIR for your STF

--- HoneyPatcher.py
+++ HoneyPatcher.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 from guizero import App, PushButton, Text
 from configparser import ConfigParser
-import tkinter as tk
-from tkinter import filedialog
 import os
 import sys
 config = ConfigParser()
@@ -10,7 +8,8 @@ config.read('HoneyConfig.ini')
 
 # VARS
 
-
+path = config.get('main', 'path')
+print(path)
 logoskip = config.getboolean('main', 'logoskip')
 
 # DEFS
@@ -33,14 +32,25 @@ def toggle_logoskip():
         config.set('main', 'logoskip', 'true')
     honey_restart()
 
+def set_directory():
+    #global directory
+    directory = app.select_folder(title="Select Sonic The Fighters USRDIR", folder=".")
+    config.set('main', 'path', directory)
+    honey_restart()
+
 # GUI
 
-logoskip = config.getboolean('main', 'logoskip')
+#logoskip = config.getboolean('main', 'logoskip')
 
-app = App(title="HoneyPatcher: Arcade Stage")
+app = App(title="HoneyPatcher: Arcade Stage", bg="#090F10")
 
 message = Text(app, text=f"Logoskip: {logoskip}")
+message.text_color = "#e7e7e7"
 
 button = PushButton(app, text="Toggle Logoskip (Disables n_advstf.farc customization)", command=toggle_logoskip)
+button.text_color = "#e7e7e7"
+
+select_folder_button = PushButton(app, text="Select STF USRDIR", command=set_directory)
+select_folder_button.text_color = "#e7e7e7"
 
 app.display()
