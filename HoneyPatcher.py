38d09d1e264f82047ff3b12d826470ee49afa3d1
coatlessali <coatlessali@protonmail.com>
2024-09-24 18:51:40 -0500

    Add backup function

--- HoneyPatcher.py
+++ HoneyPatcher.py
@@ -6,32 +6,60 @@ import os
 import sys
 import shutil
 
-# INIT
+### INIT
+# Stuff that always should run on startup
 
-configfile = Path("HoneyConfig.ini")
-if not configfile.is_file():
-    shutil.copy("HoneyConfig.default.ini", "HoneyConfig.ini")
+if not Path("HoneyConfig.ini").is_file(): # Check if config exists
+    shutil.copy("HoneyConfig.default.ini", "HoneyConfig.ini") # If not, make a new one
 config = ConfigParser()
-config.read('HoneyConfig.ini')
+config.read('HoneyConfig.ini') # Read config file
 
-# VARS
-
+### VARS
 usrdir = config.get('main', 'usrdir')
 print(usrdir)
 logoskip = config.getboolean('main', 'logoskip')
 
-# DEFS
+### DEFS
 
 def honey_write():
     with open('HoneyConfig.ini', 'w') as configfile:
         config.write(configfile)
 
+#
 def honey_restart():
     honey_write()
     app.info("info", "HoneyPatcher will now restart to apply changes.")
     os.execv(sys.executable, ['python'] + sys.argv)
 
+# TODO: Will backup game files.
+def honey_backup():
+    rom = os.path.join(usrdir, "rom.psarc")
+    if not Path(rom).is_file():
+        app.error("Hey!", "Your rom.psarc is missing! This indicates your game has been modified. Please install a clean copy of the game before backing up.")
+    else:
+        confirm_backup = app.yesno("Warning!", "Please make sure you have a CLEAN copy of Sonic The Fighters.\nIf you\'re using RPCS3, you can simply delete the game and reinstall your PKG file.\n\nAre you sure you have a clean copy of the game, and want to continue?")
+        if confirm_backup:
+            if os.path.exists("BACKUP"):
+                shutil.rmtree("BACKUP")
+            try:
+                shutil.copytree(usrdir, "BACKUP")
+            except:
+                app.error("Oopsies", "An error occured. This really shouldn't be possible unless permissions are messed up, but anyways...\n\n Ping Ali with this error: backup copy failed")
+            else:
+                app.info("Notice", "Backup complete! It will be in a folder named \"BACKUP\" (Please don't mess with it.)")
 
+# TODO: Will be used to restore game files.
+def honey_restore():
+    pass
+
+# TODO: will be used to prep the game files for modding.
+# Will require a psarc tool, and a farc tool.
+# psarc tool prob won't be too hard, farc tool is a different story
+# maybe implement the ability to target an already existing farcpack installation and wrapper?
+def honey_prep():
+    pass
+
+# Handles whether or not to patch the eboot for logoskip
 def toggle_logoskip():
     print("logoskip button pressed")
     if logoskip == True:
@@ -40,15 +68,14 @@ def toggle_logoskip():
         config.set('main', 'logoskip', 'true')
     honey_restart()
 
+# Should be set to the USRDIR, for US this is dev_hdd0/game/NPUB30927/USRDIR
 def set_directory():
     #global directory
     directory = app.select_folder(title="Select Sonic The Fighters USRDIR", folder=usrdir)
     config.set('main', 'usrdir', directory)
     honey_restart()
 
-# GUI
-
-#logoskip = config.getboolean('main', 'logoskip')
+### GUI
 
 app = App(title="HoneyPatcher: Arcade Stage", bg="#090F10")
 
@@ -62,5 +89,8 @@ button.text_color = "#e7e7e7"
 
 select_folder_button = PushButton(app, text="Select USRDIR...", command=set_directory)
 select_folder_button.text_color = "#e7e7e7"
+
+backup_button = PushButton(app, text="Backup USRDIR...", command=honey_backup)
+backup_button.text_color = "#e7e7e7"
 
 app.display()
