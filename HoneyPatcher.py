45eb4eec3b0206b09280acd09787bcac2d2af210
coatlessali <coatlessali@protonmail.com>
2024-09-24 19:13:29 -0500

    Add restore function, fix a backup bug (I think)

--- HoneyPatcher.py
+++ HoneyPatcher.py
@@ -21,23 +21,24 @@ logoskip = config.getboolean('main', 'logoskip')
 
 ### DEFS
 
+# Writes the config. Used automatically in honey_restart()
 def honey_write():
     with open('HoneyConfig.ini', 'w') as configfile:
         config.write(configfile)
 
-#
+# Acts as a way to reload the config.
 def honey_restart():
     honey_write()
     app.info("info", "HoneyPatcher will now restart to apply changes.")
     os.execv(sys.executable, ['python'] + sys.argv)
 
-# TODO: Will backup game files.
+# Backs up your USRDIR.
 def honey_backup():
     rom = os.path.join(usrdir, "rom.psarc")
     if not Path(rom).is_file():
         app.error("Hey!", "Your rom.psarc is missing! This indicates your game has been modified. Please install a clean copy of the game before backing up.")
     else:
-        confirm_backup = app.yesno("Warning!", "Please make sure you have a CLEAN copy of Sonic The Fighters.\nIf you\'re using RPCS3, you can simply delete the game and reinstall your PKG file.\n\nAre you sure you have a clean copy of the game, and want to continue?")
+        confirm_backup = app.yesno("Warning!", "Please make sure you have a CLEAN copy of Sonic The Fighters.\nIf you\'re using RPCS3, you can simply delete the game and reinstall your PKG file.\n\nAre you sure you have a clean copy of the game, and want to continue? This will take up ~86 MB of additional storage.")
         if confirm_backup:
             if os.path.exists("BACKUP"):
                 shutil.rmtree("BACKUP")
@@ -48,9 +49,24 @@ def honey_backup():
             else:
                 app.info("Notice", "Backup complete! It will be in a folder named \"BACKUP\" (Please don't mess with it.)")
 
-# TODO: Will be used to restore game files.
+# Restores your USRDIR backup.
 def honey_restore():
-    pass
+    if not os.path.exists("BACKUP"):
+        app.error("You don\'t have a backup available!")
+        return
+    confirm_restore = app.yesno("Warning!", "This will erase your USRDIR and restore a backup. This will effectively uninstall all of your USRDIR mods and restore the game to a vanilla state.\n\nDo you want to continue?")
+    if confirm_restore:
+        if os.getcwd() == usrdir:
+            app.error("What?")
+            return
+        if os.path.exists(usrdir):
+            shutil.rmtree(usrdir)
+        try:
+            shutil.copytree("BACKUP", usrdir)
+        except:
+            app.error("Oopsies", "write error here later")
+        else:
+            app.info("Notice", "Restore complete!")
 
 # TODO: will be used to prep the game files for modding.
 # Will require a psarc tool, and a farc tool.
@@ -92,5 +108,8 @@ select_folder_button.text_color = "#e7e7e7"
 
 backup_button = PushButton(app, text="Backup USRDIR...", command=honey_backup)
 backup_button.text_color = "#e7e7e7"
+
+restore_button = PushButton(app, text="Restore USRDIR...", command=honey_restore)
+restore_button.text_color = "#e7e7e7"
 
 app.display()
