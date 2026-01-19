e34b07f0555dcbd15853adfc75fc6a7f332dcc4b
coatlessali <coatlessali@protonmail.com>
2026-01-18 22:29:54 -0600

    move title to align automatically

--- gd/title.gd
+++ gd/title.gd
@@ -4,6 +4,6 @@ var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	var magic_number = rng.randi_range(1, 10)
-	text = "HoneyPatcher: Arcade Stage 8 Infinity"
+	text = "HoneyPatcher: Arcade Stage 8 Infinity +B"
 	if magic_number == 1:
-		text = "HoneyBadger: Arcade Stage 8 Infinity"
+		text = "HoneyBadger: Arcade Stage 8 Infinity +B"
