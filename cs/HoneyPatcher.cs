b273ae47c5c426fb14172cec8475cadb7be8f421
coatlessali <coatlessali@protonmail.com>
2026-08-26 13:34:09 -0500

    add libHoneyBadger and move DDS stuff to use it

--- cs/HoneyPatcher.cs
+++ cs/HoneyPatcher.cs
@@ -24,6 +24,7 @@ using LibSTF;
 using IniParser;
 using IniParser.Model;
 using UnPSARC;
+using libHoneyBadger;
 
 public partial class HoneyPatcher : Node2D
 {	
@@ -56,12 +57,12 @@ public partial class HoneyPatcher : Node2D
 	[Export] public CheckButton _cleanup;
 	[Export] public ItemList _enabledmods;
 	[Export] public ItemList _disabledmods;
-	
+	/*
 	// Fixed it?
 	byte[] ddscomp = {0x07, 0x10, 0x00, 0x00};
 	byte[] d5comp = new byte[108];
 	byte[] nocomp = new byte[108];
-	
+	*/
 	// Static directories
 	string modsDir = ProjectSettings.GlobalizePath("user://mods");
 	string workbenchDir = ProjectSettings.GlobalizePath("user://workbench");
@@ -106,10 +107,10 @@ public partial class HoneyPatcher : Node2D
 		_closemodslist.Pressed += CloseModsList;
 		_enableall.Pressed += EnableAll;
 		_disableall.Pressed += DisableAll;
-		d5comp[56] = 0x20; d5comp[60] = 0x04; d5comp[64] = 0x44; d5comp[65] = 0x58; 
-		d5comp[66] = 0x54; d5comp[67] = 0x35; d5comp[89] = 0x10;
-		nocomp[56] = 0x20; nocomp[60] = 0x41; nocomp[68] = 0x20; nocomp[74] = 0xFF; nocomp[77] = 0xFF;
-		nocomp[80] = 0xFF; nocomp[87] = 0xFF; nocomp[88] = 0x02; nocomp[89] = 0x10;
+		//d5comp[56] = 0x20; d5comp[60] = 0x04; d5comp[64] = 0x44; d5comp[65] = 0x58; 
+		//d5comp[66] = 0x54; d5comp[67] = 0x35; d5comp[89] = 0x10;
+		//nocomp[56] = 0x20; nocomp[60] = 0x41; nocomp[68] = 0x20; nocomp[74] = 0xFF; nocomp[77] = 0xFF;
+		//nocomp[80] = 0xFF; nocomp[87] = 0xFF; nocomp[88] = 0x02; nocomp[89] = 0x10;
 		
 		
 		/* Enable portable mode because people kept asking for it. */
@@ -868,30 +869,13 @@ public partial class HoneyPatcher : Node2D
 	}
 	
 	private void DDSFixHeader(){
-		string[] ddsList = Directory.EnumerateFiles(usrdir, "*.dds", SearchOption.AllDirectories).ToArray();
-		foreach (string dds in ddsList){
-			using (FileStream fs = File.Open(dds, FileMode.Open, System.IO.FileAccess.ReadWrite, FileShare.ReadWrite)){
-				byte[] headerbytes = new byte[3];
-				fs.Read(headerbytes, 0, 3);
-				string header = System.Text.Encoding.UTF8.GetString(headerbytes, 0, 3);
-				if (header != "DDS"){
-					HoneyLog(2, $"DDS file {dds} has invalid header magic. Skipping.");
-					continue;
-				}
-				fs.Seek(8, SeekOrigin.Begin);
-				fs.Write(ddscomp);
-				if (Path.GetFileName(dds).Contains("d5comp")){
-					fs.Seek(20, SeekOrigin.Begin);
-					fs.Write(d5comp);
-				}
-				else if (Path.GetFileName(dds).Contains("nocomp")){
-					fs.Seek(20, SeekOrigin.Begin);
-					fs.Write(nocomp);
-				}
-				else
-					HoneyLog(2, $"Could not determine compression type of file {dds}. Skipping.");
-			}
-		}
+		string[] ddsList = Directory.EnumerateFiles(usrdir, "*d5comp*.dds", SearchOption.AllDirectories).ToArray();
+		var badger = new HoneyBadger();
+		if (!badger.DDSFixHeader(ddsList, true))
+			HoneyLog(2, "An invalid dds header was found. Skipping.");
+		ddsList = Directory.EnumerateFiles(usrdir, "*nocomp*.dds", SearchOption.AllDirectories).ToArray();
+		if (!badger.DDSFixHeader(ddsList, false))
+			HoneyLog(2, "An invalid dds header was found. Skipping.");
 		HoneyLog(3, "Sanitized DDS headers.");
 	}
 	
