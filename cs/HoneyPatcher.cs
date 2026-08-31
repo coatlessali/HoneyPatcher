782b1700fbfedf5ade0b4873bf21cf322c8b4ff4
coatlessali <coatlessali@protonmail.com>
2026-08-30 19:36:47 -0500

    uhhhhhhh

--- cs/HoneyPatcher.cs
+++ cs/HoneyPatcher.cs
@@ -57,12 +57,7 @@ public partial class HoneyPatcher : Node2D
 	[Export] public CheckButton _cleanup;
 	[Export] public ItemList _enabledmods;
 	[Export] public ItemList _disabledmods;
-	/*
-	// Fixed it?
-	byte[] ddscomp = {0x07, 0x10, 0x00, 0x00};
-	byte[] d5comp = new byte[108];
-	byte[] nocomp = new byte[108];
-	*/
+	
 	// Static directories
 	string modsDir = ProjectSettings.GlobalizePath("user://mods");
 	string workbenchDir = ProjectSettings.GlobalizePath("user://workbench");
@@ -107,10 +102,6 @@ public partial class HoneyPatcher : Node2D
 		_closemodslist.Pressed += CloseModsList;
 		_enableall.Pressed += EnableAll;
 		_disableall.Pressed += DisableAll;
-		//d5comp[56] = 0x20; d5comp[60] = 0x04; d5comp[64] = 0x44; d5comp[65] = 0x58; 
-		//d5comp[66] = 0x54; d5comp[67] = 0x35; d5comp[89] = 0x10;
-		//nocomp[56] = 0x20; nocomp[60] = 0x41; nocomp[68] = 0x20; nocomp[74] = 0xFF; nocomp[77] = 0xFF;
-		//nocomp[80] = 0xFF; nocomp[87] = 0xFF; nocomp[88] = 0x02; nocomp[89] = 0x10;
 		
 		
 		/* Enable portable mode because people kept asking for it. */
