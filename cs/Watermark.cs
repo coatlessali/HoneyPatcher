f2b041340039b33861d9e5aaa531fc866cb0c7e2
coatlessali <coatlessali@protonmail.com>
2026-09-13 18:34:56 -0500

    clean up a debug print (oops)

--- cs/Watermark.cs
+++ cs/Watermark.cs
@@ -8,7 +8,6 @@ public partial class Watermark : Sprite2D{
 	public override void _Ready(){
 		rng.Randomize();
 		int magicNumber = rng.RandiRange(1, 69420);
-		GD.Print("a");
 		if (magicNumber == 67 && OS.GetName() == "Windows"){
 			Progress.AppendText("[W] Could not validate license. Please go to settings and activate HoneyPatcher.\n");
 			Visible = true;
